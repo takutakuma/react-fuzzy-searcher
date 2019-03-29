fbb5823c3324cca811bb1c361e9847b413f66f0c
takutakuma <tischwahrheit@gmail.com>
2019-03-30 07:06:54 +0900

    libを追加

---
+++ lib/SearchDropdown.d.ts
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Theme, WithStyles } from '@material-ui/core/styles';
+import { ISearchResultOptions } from './SearchResult';
+declare const styles: (theme: Theme) => Record<"root" | "dropdownContainer" | "grow" | "menuItem" | "noResultsText", import("@material-ui/core/styles/withStyles").CSSProperties>;
+export interface ISearchDropdownProps extends WithStyles<typeof styles> {
+    showDropdown: boolean;
+    anchorEl: HTMLDivElement | null;
+    handleDropdownClose(event: any): void;
+    data: any;
+    maxDropdownHeight: string;
+    searchResultOptions?: ISearchResultOptions;
+}
+declare const _default: React.ComponentType<Pick<ISearchDropdownProps, "maxDropdownHeight" | "searchResultOptions" | "showDropdown" | "data" | "anchorEl" | "handleDropdownClose"> & import("@material-ui/core").StyledComponentProps<"root" | "dropdownContainer" | "grow" | "menuItem" | "noResultsText">>;
+export default _default;
